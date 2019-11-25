3e01c46fc884e923757f81d9c9d8999e268fa025
hex <castronet@gmail.com>
2019-11-25 10:01:44 +0100

    Adding TLS configuration to https server and now it's operational.

--- src/webapp/webapp.go
+++ src/webapp/webapp.go
@@ -1,30 +1,26 @@
 package main
 
 import (
-	"fmt"
+//	"fmt"
 	"net/http"
-	"os"
+	"io"
 	"log"
-)
-
-const (
-//	DEBUG = false
-	DEBUG = true
 )
 
 // Main function
 func main() {
+	// Handle specific request
+	// Set the configuration before start the server
+	http.HandleFunc("/hello", handleRequest);
 
 	// Create server and listen on desired port with TLS
 	// The certificate is self signed. A good idea is to use Let's Encrypt certificates.
-	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil)
 	log.Fatal(err)
-
-	// Handle specific request
-	http.HandleFunc("/hello", handleRequest);
 }
 
 // Handle incoming requests
 func handleRequest(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "{ \"hello\": \"world\" }")
+	// As requested this endopoint just has to answer hello world
+	io.WriteString(w, "{ \"hello\": \"world\" }")
 }
