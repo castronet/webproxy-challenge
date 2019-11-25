4faff472f50c1f18ba60c2e110478218f076697f
hex <castronet@gmail.com>
2019-11-25 09:07:37 +0100

    Added first initial version.

---
+++ src/webapp/webapp.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+//	DEBUG = false
+	DEBUG = true
+)
+
+// Main function
+func main() {
+}
+
+// Handle incoming requests
+func handleRequest() {
+}
